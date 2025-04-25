e413e6118487b206556942941221d6c6df4e5bb8
furlanandraz <andraz.furlan98@gmail.com>
2025-04-25 21:37:23 +0200

    container utils class added, Header styled

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -1,18 +1,28 @@
 import { useCharacters } from "../../context/CharactersContext";
+import clsx from "clsx";
 
 import style from './Header.module.css';
+import Logo from '../../static/Logo.png';
+
+import '../../App.css';
 
 export default function Header() {
 
-    const { setFilterStatus } = useCharacters();
+    const { filterStatus, setFilterStatus } = useCharacters();
     
     return (
         <header className={style.header}>
-            <nav className={style.navbar}>
-                <button onClick={() => setFilterStatus('')}>All</button>
-                <button onClick={() => setFilterStatus('alive')}>Alive</button>
-                <button onClick={() => setFilterStatus('dead')}>Dead</button>
-            </nav>
+            <div className="container">
+                <div className={style.wrapper}>
+                    <img className={style.logo} src={Logo} alt="" />
+                    <nav className={style.nav}>
+                        <button className={clsx(style.button, {[style.active]: filterStatus === ''})} onClick={() => setFilterStatus('')}>All</button>
+                        <button className={clsx(style.button, {[style.active]: filterStatus === 'alive'})} onClick={() => setFilterStatus('alive')}>Alive</button>
+                        <button className={clsx(style.button, {[style.active]: filterStatus === 'dead'})} onClick={() => setFilterStatus('dead')}>Dead</button>
+                    </nav>
+                </div>
+            </div>
+            
         </header>
     );
 }
