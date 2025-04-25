00feba04b733a8cf397db9087c66888c40556cc1
furlanandraz <andraz.furlan98@gmail.com>
2025-04-25 21:47:52 +0200

    breadcrumbs styled

--- src/ui/components/Breadcrumbs.jsx
+++ src/ui/components/Breadcrumbs.jsx
@@ -3,10 +3,18 @@ import style from './Breadcrumbs.module.css';
 export default function Breadcrumbs({ breadcrumbs }) {
 
     return (
+
         <nav aria-label="breadcrumb" className={style.breadcrumbs}>
-            {breadcrumbs.map((breadcrumb, i) => 
-                <button onClick={breadcrumb.onClick} disabled={i === breadcrumbs.length - 1}>{breadcrumb.label}</button>
-            )}
+            <div className="container">
+                <div className={style.wrapper}>
+                    {breadcrumbs.map((breadcrumb, i) => 
+                        <>
+                            <button key={breadcrumb.label} className={style.breadcrumb} onClick={breadcrumb.onClick} disabled={i === breadcrumbs.length - 1}>{breadcrumb.label}</button>
+                            <span className={style.separator}></span>
+                        </>
+                    )}
+                </div>
+            </div>
         </nav>
     );
 }
